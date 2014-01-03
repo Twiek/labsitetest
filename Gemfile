97b790da266bbaf437069099ba00dbac501376de
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2014-01-03 22:53:58 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.16'
+gem 'bootstrap-sass'
+gem 'sqlite3'
+gem 'haml'
+gem "haml-rails"
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'factory_girl_rails'
+end
+
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem "font-awesome-rails"
+  gem 'uglifier'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Putzen::Application.config.secret_token = 'd7a92f8ab0ce62e89bc34213551ee0337118136cd8412905762936fb4b2ec539e08b2ca8def5e61903af127e382e27c3ec414c3e7ac510af0494a0c43fae1fa6'
