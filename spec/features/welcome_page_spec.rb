e68dc20c0fecca5290d9c1785c51bd3736d43f15
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2014-01-20 23:31:57 +0100

    Cleanup

--- spec/features/welcome_page_spec.rb
+++ spec/features/welcome_page_spec.rb
@@ -7,7 +7,7 @@ describe "Welcome Page" do
   describe "Home Page" do
     before { visit root_path}
 
-    it "should have a link to the home page", visual: true do
+    it "should have a link to the home page" do
       find("img[@alt='Logo-small']").click
       should have_content("Jetzt putzen lassen!")
       should have_selector(".offers-price-image")
